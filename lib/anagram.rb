26a5cd194fec29c003632aeb05f3b6c902b3b197
Brad Schrag <brad.schrag@gmail.com>
2018-11-19 18:17:55 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -2,22 +2,22 @@
 class Anagram
   attr_accessor :word
 
-  def self.match(list_of_words)
-    self_dictionary = self.word_to_dictionary(@word)
+  def initialize(word)
+    @word = word
+  end
+
+  def match(list_of_words)
+    self_dictionary = word_to_dictionary(@word)
     match_list = []
     list_of_words.each do | word |
-      if self.word_to_dictionary(word) == self_dictionary
+      if word_to_dictionary(word) == self_dictionary
         match_list << word
       end
     end
     match_list
   end
 
-  def initialize(word)
-    @word = word
-  end
-
-  def self.word_to_dictionary(word)
+  def word_to_dictionary(word)
     dictionary = {}
     for letter in word.split('')
       if dictionary.include?(letter)

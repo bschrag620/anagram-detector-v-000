8575a11a40cac6d73ad1ca76e51c3fbd1556e5a6
Brad Schrag <brad.schrag@gmail.com>
2018-11-19 17:58:26 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,29 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+    @lettercount = self.word_to_dictionary(@word)
+  end
+
+  def self.word_to_dictionary(word)
+    dictionary = {}
+    for letter in word.split('')
+      if dictionary.include?(letter)
+        dictionary[letter] += 1
+      else
+        dictionary[letter] = 1
+      end
+    end
+    dictionaty
+  end
+
+  def self.match(list_of_words)
+    list_of_words.collect do | word |
+      if self.word_to_dictionary(word) == @lettercount
+        word
+      end
+    end
+  end
+  
